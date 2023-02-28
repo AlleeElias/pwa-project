04098f22013eedcdfca02fc1172123910b624d6e
AlleeElias <eliasneel2001@gmail.com>
2023-02-28 12:19:54 +0100

    offline storage

--- src/services/firestore.js
+++ src/services/firestore.js
@@ -1,5 +1,8 @@
 import {initializeApp} from "firebase/app";
 import {getFirestore} from 'firebase/firestore';
+import {enableIndexedDbPersistence} from "firebase/firestore";
+import {initializeFirestore, CACHE_SIZE_UNLIMITED} from "firebase/firestore";
+
 
 const firebaseConfig = {
     projectId: "projectdynamic-c443e",
@@ -7,5 +10,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
 
-export const firestoreDB = getFirestore(firebaseApp);
+export const firestoreDB = initializeFirestore(firebaseApp, {cacheSizeBytes: CACHE_SIZE_UNLIMITED});
+enableIndexedDbPersistence(firestoreDB).then(r => console.log("persisted"));
 console.log("initialized firebase connection");
